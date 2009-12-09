3e7e1a7d056d07e5d49fe660a0a37b0dcf3454ec
Nguyen.Phuc.X <Nguyen.Phuc.X@da00125e-e2ec-11de-a155-4d945b6d75ec>
2009-12-09 00:21:08 +0000

    Added handling join_reponse - Not complete

--- trunk/app/p2p.h
+++ trunk/app/p2p.h
@@ -6,6 +6,11 @@ unsigned int calc_p2p_id(unsigned char *s){
   return hashval % 1024;
 }
 
+// Check if the given p2p_id is in range r, inclusively
+unsigned int isInRange(int p2p_id, Range * r){
+  return (p2p_id >= r->low) && (p2p_id <= r->high);
+}
+
 unsigned int handle_sendjoin(int sock,int myID){
 	struct header *hdr=(struct header *) malloc(sizeof(int));
 	struct p2p_join_request *jr=(struct p2p_join_request *) malloc(sizeof(struct p2p_join_request));
@@ -23,6 +28,35 @@ unsigned int handle_sendjoin(int sock,int myID){
 	memcpy(tosent+4,payload_c,4);
 
 	int bytes_sent = send(sock,tosent,8,0);
+
+	if( bytes_sent < 0 ){
+		perror("send failed");
+	}
+
+	free(hdr);
+	free(jr);
+	free(tosent);
+}
+
+// Handling sending join response
+unsigned int handle_sendjoinresponse(int sock,unsigned int count,unsigned char * userdata){
+	struct header *hdr=(struct header *) malloc(sizeof(int));
+	struct p2p_join_response *jr=(struct p2p_join_response *) malloc(sizeof(char)*(count*20+4));
+	hdr->version = 0x04;
+	hdr->len = htons(count*20+4+4); // userdata = count*20; usernumber = 4; header = 4
+	hdr->msgtype = htons(0x11);
+
+	jr->usernumber = htnol(count);
+
+	unsigned char * payload_c = (unsigned char*) jr;
+	unsigned char * header_c = (unsigned char*) hdr;
+	unsigned char * tosent = (unsigned char *) malloc(sizeof(hdr->len));
+
+	memcpy(tosent,header_c,4);
+	memcpy(tosent+4,payload_c,4);
+	memcpy(tosent+8,userdata,count*20);
+
+	int bytes_sent = send(sock,tosent,hdr->len,0);
 
 	if( bytes_sent < 0 ){
 		perror("send failed");
